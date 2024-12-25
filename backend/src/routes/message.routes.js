d4562dbde4516a6e1d1a209feac029a03069571f
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:44:55 +0530

    Update message.routes.js

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const {getUsersForSidebar,getMessages,sendMessages} = require("../controllers/message.controller.js");
+const protectRoute = require("../middlewares/auth.middleware");
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id",protectRoute,getMessages);
+router.post("/send/:id",protectRoute,sendMessages);
+module.exports = router;
 
