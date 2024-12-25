d81dde9ccc7ef33aa8981b905bcb15c5ba0459cc
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:44:38 +0530

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,1 +1,16 @@
 
+const express = require("express");
+const router = express.Router();
+const { logIn, signUp, logOut,updateProfile,checkAuth } = require("../controllers/auth.controller.js");
+const protectRoute = require("../middlewares/auth.middleware.js")
+
+router.post("/signup", signUp);
+
+router.post("/login", logIn);
+
+router.post("/logout", logOut);
+
+router.put("/updateprofile", protectRoute,updateProfile);
+router.get("/checkAuth",protectRoute,checkAuth);
+
+module.exports = router;
