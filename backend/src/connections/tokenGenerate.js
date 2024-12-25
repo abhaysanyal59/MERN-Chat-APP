b1bf670888a5da4ec2feeb6a6e16d1575615be5e
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:42:27 +0530

    Update tokenGenerate.js

--- backend/src/connections/tokenGenerate.js
+++ backend/src/connections/tokenGenerate.js
@@ -1,1 +1,16 @@
+const jwt = require("jsonwebtoken");
 
+const generateToken = (userId,response) => {
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"}) // jwt.sign creates a token
+response.cookie("jwt",token,
+    {
+        maxAge: 7*24*60*60*1000, // 7 days in milliseconds
+        httpOnly:true , // only acccessible to the browser
+        sameSite:"strict",
+        secure:process.env.NODE_ENV !== "development"
+    }
+)
+return token;
+}
+
+module.exports = generateToken;
