efb2115be00caa2d49fabb5ef9e5bb82a481f066
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:44:02 +0530

    Update message.models.js

--- backend/src/models/message.models.js
+++ backend/src/models/message.models.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+const messageModel = mongoose.model("Message", messageSchema);
+
+module.exports = messageModel;
 
