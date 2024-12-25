5783caeffab27ef197fdb46a9b4acc96d4ab38ae
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:48:46 +0530

    Update Axios.js

--- frontend/src/lib/Axios.js
+++ frontend/src/lib/Axios.js
@@ -1,1 +1,14 @@
+import axios from "axios";
 
+const baseURL =
+  process.env.REACT_APP_API_URL || // Custom API URL for development
+  (process.env.NODE_ENV === "development" 
+    ? "http://localhost:5001/api" // Default for local development
+    : "/api"); // Relative path for production
+
+export const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+console.log("Axios baseURL:", baseURL); // Debugging log
