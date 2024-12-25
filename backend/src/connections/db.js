35a78744b3d9bfcf0a906d4cc1ec623b103f2c90
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:41:40 +0530

    Update db.js

--- backend/src/connections/db.js
+++ backend/src/connections/db.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv");
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Connection established at ${connect.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection Error",error);
+  }
+};
+
+module.exports = connectDb;
 
