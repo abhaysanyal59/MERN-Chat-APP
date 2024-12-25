4205ab6a28aceb0dd4a08f50f4e7d3be18fe5c28
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:43:37 +0530

    Update auth.middleware.js

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,1 +1,31 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.models.js");
+
+const protectRoute = async (request, response, next) => {
+  try {
+    const token = request.cookies.jwt;
+    console.log("Request Cookies:",request.cookies);
+    if (!token) {
+      return response
+        .status(401)
+        .json({ message: "Unauthorized Access - No Token" });
+    }
+  
+  const decodedToken = await jwt.verify(token,process.env.JWT_SECRET)
+if(!decodedToken) {
+    return response.status(401).json({message:"Unauthorized Access - Invalid Token"})
+}
+const user  = await userModel.findById(decodedToken.userId).select("-password")
+if(!user) {
+    return response.status(404).json({message:"User Not Found"})
+}
+request.user = user;
+next();
+
+}
+catch (error) {
+    console.log("Error Occured Here",error.message);
+  }
+}
+module.exports = protectRoute;
 
