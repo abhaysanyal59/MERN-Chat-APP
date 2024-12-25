ca14afc52ebe4b798839245bbbaee552a52b9297
abhaysanyal59 <abhaysanyal59@gmail.com>
2024-12-25 19:44:19 +0530

    Update user.models.js

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: { type: String, required: true },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+const userModel = mongoose.model("User",userSchema);
+
+module.exports = userModel;
